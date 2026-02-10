ec0b0908c9275b90a700356c0c556e0564b93c16
Kawa-shima <comkyowu@gmail.com>
2026-02-10 11:13:17 +0900

    first commit

---
+++ database/migrations/2026_02_09_153000_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('author')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->foreignId('genre_id')->constrained();
+            $table->foreignId('setting_id')->constrained();
+            $table->foreignId('magazine_id')->constrained();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
